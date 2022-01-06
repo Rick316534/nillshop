9379c61c23fb9098b1d2878de66737a0affeb30e
nill <nill@mail.chungyo.net>
2022-01-06 18:37:01 +0800

    編輯商品_2

---
+++ admin/database/migrations/2022_01_06_103218_create_returns_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReturnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('returns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('order_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('returns');
+    }
+}
